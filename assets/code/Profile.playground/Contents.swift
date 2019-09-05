3370702b30bc90f5cfe3de85f573ca1bcf237e07
CJ Lin <ejames.lin@gmail.com>
2019-09-06 00:30:28 +0800

    First slide using remarkjs

---
+++ assets/code/Profile.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Profile {
+    let name = "CJ Lin"
+    var identity = "iOS Developer"
+    var company = "LINE"
+    var blog = "https://ejameslin.github.io"
+    var line = "ejameslin"
+    var twitter = "@eJamesLin"
+}
